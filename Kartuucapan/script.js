13956c533f38327dd648975c5c5feab5e737c67d
FasyaIrfan23 <fasyairfan2304@gmail.com>
2024-10-05 17:33:43 +0700

    Add files via upload

---
+++ Kartuucapan/script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    const message = document.getElementById('special-message');
+    message.textContent = "Semoga menjadi anak BUMN ya, Semangat Terusss!";
+    message.style.color = "#ff6347";
+}
